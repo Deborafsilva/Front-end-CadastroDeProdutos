03a8e986f693d8ba8e3c56c14d9664f3772ce66c
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-08 15:39:43 -0300

    Criando a estrutura do formulario para: cadastrar, alterar ,remover, cancelar

--- src/formulario/Formulario.js
+++ src/formulario/Formulario.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Formulario() {
   return (
-    <div>
-      <h1>Formulário</h1>
-    </div>
+    <form>
+      <input type='text' placeholder='Nome'/>
+      <input type='text' placeholder='Marca'/>
+
+      <input type='button' value='Cadastrar'/>
+      <input type='button' value='Alterar'/>
+      <input type='button' value='Remover'/>
+      <input type='button' value='Cancelar'/>
+    </form>
   )
 }

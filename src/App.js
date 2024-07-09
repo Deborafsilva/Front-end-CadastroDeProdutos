41727efbaccadfd5b9e0a9d5053e447c7f341211
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-09 14:17:25 -0300

    Requisição de API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './formulario/Formulario';
 import Tabela from './tabela/Tabela';
@@ -7,9 +7,18 @@ import Tabela from './tabela/Tabela';
 function App() {
 
   const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno=> retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+
+  },[]);
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela/>
     </div>
